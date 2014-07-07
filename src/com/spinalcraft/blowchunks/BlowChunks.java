447a1c3b388f47332d1c944902a269ee5820a0ec
Parker Kemp <parker.kemp@hitcents.com>
2014-07-07 12:43:51 -0500

    Delete chunks listed in DB.

--- src/com/spinalcraft/blowchunks/BlowChunks.java
+++ src/com/spinalcraft/blowchunks/BlowChunks.java
@@ -1,6 +1,16 @@
 package com.spinalcraft.blowchunks;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
 public class BlowChunks {
+	
+	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+	static final String DB_URL = "jdbc:mysql://localhost";
+	static Connection conn = null;
 	
 	public static void main(String[] args){
 		if(args.length < 1){
@@ -8,11 +18,36 @@ public class BlowChunks {
 			return;
 		}
 		
-		String regionDir = args[0];
+		Path regionDir = Paths.get(args[0]);
 		
-		ChunkDeleter deleter = new ChunkDeleter(regionDir);
+		connectToDatabase();
 		
-		//deleter.deleteChunk(5, 2);
-		//System.out.println("Deleted a chunk.");
+		ChunkDeleter deleter = new ChunkDeleter(conn, regionDir);
+		
+		switch(deleter.deleteChunksFromDB()){
+		case 0:
+			System.out.println("Successfully deleted chunks!");
+			break;
+		case 1:
+			System.out.println("SQL error.");
+			break;
+		}
+	}
+	
+	private static void connectToDatabase(){
+		String query;
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			conn = DriverManager.getConnection(DB_URL, "root", "password");
+			Statement stmt = conn.createStatement();
+			stmt = conn.createStatement();
+
+			query = "USE Spinalcraft";
+			stmt.executeQuery(query);
+		
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
--- src/com/spinalcraft/blowchunks/ChunkDeleter.java
+++ src/com/spinalcraft/blowchunks/ChunkDeleter.java
@@ -4,27 +4,82 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+class ChunkLocation{
+	public String world;
+	public int x, z;
+	public ChunkLocation(String world, int x, int z){
+		this.world = world;
+		this.x = x;
+		this.z = z;
+	}
+}
 
 public class ChunkDeleter {
-	private String regionDirPath;
+	private Path serverFolderPath;
+	private Connection conn;
 	
-	public ChunkDeleter(String regionDirPath){
-		this.regionDirPath = regionDirPath;
+	public ChunkDeleter(Connection conn, Path serverFolderPath){
+		this.serverFolderPath = serverFolderPath;
+		this.conn = conn;
 	}
 	
-	public void deleteChunk(int chunkX, int chunkZ){
-		int regionX = (int)Math.floor(chunkX / 32.0);
-		int regionZ = (int)Math.floor(chunkZ / 32.0);
+	public int deleteChunksFromDB(){
+		String query = "SELECT world, x, z FROM Chunks";// WHERE deleted IS NULL";
+		Statement stmt;
+				
+		ArrayList<ChunkLocation> chunks = new ArrayList<ChunkLocation>();
 		
-		Path regionFilename = Paths.get(regionDirPath, "r." + regionX + "." + regionZ + ".mca");
+		try {
+			stmt = conn.createStatement();
+			ResultSet rs = stmt.executeQuery(query);
+			while(rs.next()){
+				chunks.add(new ChunkLocation(rs.getString("world"), rs.getInt("x"), rs.getInt("z")));
+			}
+			for(int i = 0; i < chunks.size(); i ++)
+				deleteChunk(chunks.get(i));
+			
+			//query = "UPDATE Chunks SET deleted = NULL WHERE deleted NOT NULL";
+			//stmt = conn.createStatement();
+			//stmt.executeUpdate(query);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return 1;
+		}
 		
-		int xPosInRegion = chunkX % 32;
-		if(chunkX < 0)
-			chunkX += 32;
-		int zPosInRegion = chunkZ % 32;
-		if(chunkZ < 0)
-			chunkZ += 32;
+		return 0;
+	}
+	
+	private void deleteChunk(ChunkLocation location){
+		int regionX = (int)Math.floor(location.x / 32.0);
+		int regionZ = (int)Math.floor(location.z / 32.0);
+		
+		Path regionDirPath = serverFolderPath;
+		
+		if(location.world.equalsIgnoreCase("world")){
+			regionDirPath = serverFolderPath.resolve("world/region");
+		}
+		else if(location.world.equalsIgnoreCase("world_nether")){
+			regionDirPath = serverFolderPath.resolve("world_nether/DIM-1/region");
+		}
+		else if(location.world.equalsIgnoreCase("world_the_end")){
+			regionDirPath = serverFolderPath.resolve("world_the_end/DIM1/region");
+		}
+		
+		Path regionFilename = regionDirPath.resolve("r." + regionX + "." + regionZ + ".mca");
+		
+		int xPosInRegion = location.x % 32;
+		if(xPosInRegion < 0)
+			xPosInRegion += 32;
+		int zPosInRegion = location.z % 32;
+		if(zPosInRegion < 0)
+			zPosInRegion += 32;
 		
 		long byteOffset = 4 * (xPosInRegion + zPosInRegion * 32);
 		byte[] zeroBytes = {0, 0, 0, 0};
@@ -41,5 +96,6 @@ public class ChunkDeleter {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		System.out.println("Deleted chunk (" + location.x + ", " + location.z + ")");
 	}
 }
